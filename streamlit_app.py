f37513716b75ba2000eec7c05a950457ff7e56e0
Yash Izate <yashizate.softech@gmail.com>
2026-07-22 12:04:55 +0530

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,19 @@ upload_file = st.file_uploader("upload CSV file", type= "csv")
 if upload_file:
     df = pd.read_csv(upload_file)
     st.dataframe(df)
+
+
+st.header("This is a Header ")
+st.subheader("This is a subheader ")
+st.markdown(" **Bold**, *Italic*, 'code', [Link] (https://streamlit.io/)")
+st.code("for i in range(5): print(i)", language="python")
+
+#inputs in streamlit
+st.text_input("What is your name")
+st.text_area("Write your message")
+st.number_input("pick a number", min_value=0, max_value=100)
+st.slider("choose a range", 0, 100)
+st.selectbox("Select a fruit", ["Apple", "Orange", "Mango"])
+st.multiselect("Choose toppings", ["Cheese","Tomato", "Olives"])
+st.radio("Pick one",["Option A", "Option B"])
+st.checkbox("I agree all terms & conditions.")
